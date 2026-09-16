817fcf5f4c750098bb1adfc8ef1e1d5b742726d3
Ritx23 <karanachi345@gmail.com>
2026-09-16 20:29:29 +0530

    Initial commit - add README update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./TURMIN-quba-gallery/"
+  base: "/TURMIN-quba-gallery/"
 })
